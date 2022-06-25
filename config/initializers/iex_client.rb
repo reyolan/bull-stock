8ab65e7923ada313abe8a7f18886ab26c3e80405
reyolan <reynardolan@gmail.com>
2022-06-25 10:03:39 +0800

    add iex gem config

---
+++ config/initializers/iex_client.rb
@@ -0,0 +1,5 @@
+IEX::Api.configure do |config|
+  config.publishable_token = Rails.application.credentials.sandbox_publishable_token
+  config.secret_token = Rails.application.credentials.sandbox_secret_token
+  config.endpoint = 'https://sandbox.iexapis.com/v1'
+end
