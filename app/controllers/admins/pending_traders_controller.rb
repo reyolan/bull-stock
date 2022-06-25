c017bf1b6cc37c3e9d6b62231f5b21b1bf746df9
reyolan <reynardolan@gmail.com>
2022-06-25 12:00:53 +0800

    create new controller for approved trader

--- app/controllers/admins/pending_traders_controller.rb
+++ app/controllers/admins/pending_traders_controller.rb
@@ -4,10 +4,4 @@ class Admins::PendingTradersController < ApplicationController
     # User Story #5
     # User.all.where(approved: :false)
   end
-
-  def destroy
-    # Admin User Story #6
-    # Trader User Story #4
-    # We can think of this as removing/destroying the pending state of the trader
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  scope module: 'admins' do
+    resources :pending_traders, only: %i[index destroy]
+  end    
 
   root 'static_pages#home'
 end
