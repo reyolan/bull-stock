146b5dc9535c18995342f67683172251a8fff0fc
reyolan <reynardolan@gmail.com>
2022-06-25 12:33:26 +0800

    add hamburger script for header

--- app/frontend/entrypoints/application.ts
+++ app/frontend/entrypoints/application.ts
@@ -2,14 +2,8 @@ import * as ActiveStorage from "@rails/activestorage";
 import Rails from "@rails/ujs";
 import Turbolinks from "turbolinks";
 import "../../assets/stylesheets/application.css";
+import "./scripts/mobile_navigation";
 
 ActiveStorage.start();
 Rails.start();
 Turbolinks.start();
-
-console.log("Vite ⚡️ Rails");
-
-console.log(
-  "Visit the guide for more information: ",
-  "https://vite-ruby.netlify.app/guide/rails"
-);
