964c50b871334fb76ab8ed42ce7018a3002eec99
reyolan <reynardolan@gmail.com>
2022-06-25 12:12:29 +0800

    add routes and edit header view

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,17 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   scope module: 'admins' do
-    resources :pending_traders, only: %i[index destroy]
-  end    
+    resources :pending_traders, only: :index
+    resources :approved_traders, only: :create
+    resources :traders
+    resources :transactions
+  end
+
+  scope module: 'traders' do
+    resources :stocks, only: %i[new create update destroy]
+    get 'portfolio', to: 'stocks#index'
+    resources :transactions, only: :index
+  end
 
   root 'static_pages#home'
 end
