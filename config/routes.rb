1935df9a7b79dac8b395bb7db27fa4f85e9199b2
reyolan <reynardolan@gmail.com>
2022-06-30 08:25:01 +0800

    edit design of flash message and error message, remove flash message in sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: { sessions: 'users/sessions' }
+  devise_scope :user do
+    get '/users', to: 'devise/registrations#new'
+  end
 
   scope module: 'admins' do
     resources :pending_traders, only: :index
