969f5eac43099af9f3566e689f467390c671cf8a
reyolan <reynardolan@gmail.com>
2022-06-30 08:45:16 +0800

    edit seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,7 @@ User.create!(email: 'approved.trader@stocks.com',
              role: 1)
 
 # not approved trader
-User.create!(email: 'approved.trader@stocks.com',
+User.create!(email: 'not.approved.trader@stocks.com',
              password: 'abc123',
              password_confirmation: 'abc123',
              confirmed_at: Time.now.utc,
