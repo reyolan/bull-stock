1698b7bbe7eb5ee7371ff59c41331c85fdc68e50
reyolan <reynardolan@gmail.com>
2022-06-30 08:44:09 +0800

    add helper and seed database

--- app/helpers/traders/transactions_helper.rb
+++ app/helpers/traders/transactions_helper.rb
@@ -1,2 +1,5 @@
 module Traders::TransactionsHelper
+  def display_purchase_or_search_stock
+    current_user.approved? ? 'Purchase Stock' : 'Search Stock'
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :stocks
   has_many :transactions
 
-  enum role: %i[trader admin]
+  enum role: %i[admin trader]
   after_initialize :set_default_role, :if => :new_record?
 
   devise :database_authenticatable, :registerable,
