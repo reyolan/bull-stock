48fb21a4df725e5824cbc3d90464c622e491e602
reyolan <reynardolan@gmail.com>
2022-07-04 00:52:34 +0800

    refactor some logic

--- app/controllers/admins/pending_traders_controller.rb
+++ app/controllers/admins/pending_traders_controller.rb
@@ -1,11 +1,8 @@
-# Admin User Stories #5 and #6
 class Admins::PendingTradersController < ApplicationController
   before_action :authenticate_admin
-  
+
   def index
-    # User Story #5
-    # User.all.where(approved: :false)
-    @q = User.where(role: "trader", approved: :false).ransack(params[:q])
+    @q = User.where(role: 'trader', approved: false).ransack(params[:q])
     @pending_traders = @q.result
   end
 end
