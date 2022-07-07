0f6b5658f1014c800145c174bfb3f1e2aaca5074
reyolan <reynardolan@gmail.com>
2022-07-07 17:48:56 +0800

    add more balance transaction model test

--- spec/factories/balance_transactions.rb
+++ spec/factories/balance_transactions.rb
@@ -3,12 +3,22 @@ FactoryBot.define do
     association :user, factory: :approved_confirmed_trader
     amount { 250 }
 
-    factory :deposit_transaction do
+    trait :deposit_type do
       transaction_type { 0 }
     end
 
-    factory :withdraw_transaction do
+    trait :withdraw_type do
       transaction_type { 1 }
     end
+
+    trait :yesterday_created do
+      created_at { 1.day.ago }
+      updated_at { 1.day.ago }
+    end
+
+    factory :deposit_transaction, traits: %i[deposit_type]
+    factory :yesterday_deposit_transaction, traits: %i[yesterday_created deposit_type]
+    factory :withdraw_transaction, traits: %i[withdraw_type]
+    factory :yesterday_withdraw_transaction, traits: %i[yesterday_created withdraw_type]
   end
 end
--- spec/models/balance_transaction_spec.rb
+++ spec/models/balance_transaction_spec.rb
@@ -8,24 +8,75 @@ RSpec.describe BalanceTransaction, type: :model do
   it 'is invalid without association to a user' do
     balance_transaction = build(:deposit_transaction, user_id: nil)
     balance_transaction.valid?
-    expect(balance_transaction.errors[:user]).to include('must exist')
+    expect(balance_transaction.errors[:user]).to(include('must exist'))
   end
 
   it 'is invalid with amount scaling to three or more' do
     balance_transaction = build(:deposit_transaction, amount: 2.225)
     balance_transaction.valid?
-    expect(balance_transaction.errors[:base]).to include('You can only input amount up to scale of 2 (e.g. 20, 20.3, 20.25)')
+    expect(balance_transaction.errors[:base]).to(include('You can only input amount up to scale of 2 (e.g. 20, 20.3, 20.25)'))
   end
 
   it 'is invalid with negative amount' do
     balance_transaction = build(:deposit_transaction, amount: -12)
     balance_transaction.valid?
-    expect(balance_transaction.errors[:base]).to include('An input of negative amount is not possible')
+    expect(balance_transaction.errors[:base]).to(include('An input of negative amount is not possible'))
   end
 
-  it 'is invalid with amount of zero' do 
+  it 'is invalid with amount of zero' do
     balance_transaction = build(:deposit_transaction, amount: 0)
     balance_transaction.valid?
-    expect(balance_transaction.errors[:base]).to include('Please input a valid amount')
+    expect(balance_transaction.errors[:base]).to(include('Please input a valid amount'))
+  end
+
+  describe '.deposits' do
+    before do
+      @deposit = create(:deposit_transaction)
+      @yesterday_deposit = create(:yesterday_deposit_transaction)
+      @withdraw = create(:withdraw_transaction)
+    end
+
+    it 'includes balance transactions with deposit transaction type' do
+      expect(BalanceTransaction.deposits).to(include(@deposit, @yesterday_deposit))
+    end
+
+    it 'has a descending order based on created_at' do
+      expect(BalanceTransaction.deposits.first).to(eq(@deposit))
+    end
+
+    it 'excludes balance transactions without deposit transaction type' do
+      expect(BalanceTransaction.deposits).not_to(include(@withdraw))
+    end
+  end
+
+  describe '.withdrawals' do
+    before do
+      @deposit = create(:deposit_transaction)
+      @withdraw = create(:withdraw_transaction)
+      @yesterday_withdraw = create(:yesterday_withdraw_transaction)
+    end
+
+    it 'includes balance transactions with withdrawal type' do
+      expect(BalanceTransaction.withdrawals).to(include(@withdraw, @yesterday_withdraw))
+    end
+
+    it 'orders the collection in descending order based on created_at attribute' do
+      expect(BalanceTransaction.withdrawals.first).to(eq(@withdraw))
+    end
+
+    it 'excludes balance transaction without withdraw transaction type' do
+      expect(BalanceTransaction.withdrawals).not_to(include(@deposit))
+    end
+  end
+
+  describe '.desc_created_at' do
+    before do
+      @today_transaction = create(:withdraw_transaction)
+      @yesterday_transaction = create(:yesterday_withdraw_transaction)
+    end
+
+    it 'orders the collection in descending order based on created_at attribute' do
+      expect(BalanceTransaction.first).to(eq(@today_transaction))
+    end
   end
 end
