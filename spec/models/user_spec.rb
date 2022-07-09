49867f2eb5aa22dca42e98d76c6b94747ae8a46c
reyolan <reynardolan@gmail.com>
2022-07-09 12:38:40 +0800

    remove unnecessary spec files

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
   let(:trader) { build(:approved_confirmed_trader) }
 
   it 'has a valid factory' do
