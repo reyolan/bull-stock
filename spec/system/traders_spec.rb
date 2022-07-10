d31c7e6a569772e6d1dff71bf226711c9caac086
reyolan <reynardolan@gmail.com>
2022-07-10 20:35:14 +0800

    add first user story system test

--- spec/system/traders_spec.rb
+++ spec/system/traders_spec.rb
@@ -1,9 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe "Traders", type: :system do
+RSpec.describe "Trader Feature", type: :system do
   before do
     driven_by(:rack_test)
   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  it 'allows creation of trader account to buy and sell stocks' do
+    visit(new_user_registration_path)
+
+    fill_in 'user_email', with: 'example@example.com'
+    fill_in 'user_password', with: '123456'
+    fill_in 'user_password_confirmation', with: '123456'
+
+    within('form') do
+      click_on('Sign up')
+    end
+
+    expect(User.all.count).to(eq(1))
+  end
 end
