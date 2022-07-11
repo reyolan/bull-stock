729edb8034b525ec9c6c401f8fa4f41bbcbac705
reyolan <reynardolan@gmail.com>
2022-07-11 12:36:13 +0800

    add new trader spec

---
+++ spec/system/admin/new_trader_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe 'Add trader', type: :system do
+  let(:admin) { create(:admin) }
+  let(:trader_to_create) { build(:unapproved_unconfirmed_trader) }
+  let(:approved_trader) { create(:approved_confirmed_trader) }
+
+  context 'when user is an admin' do
+    it 'allows creation of trader account' do
+      sign_in admin
+
+      visit new_trader_path
+
+      fill_in 'user[email]', with: trader_to_create.email
+      fill_in 'user[password]', with: trader_to_create.password
+      fill_in 'user[password_confirmation]', with: trader_to_create.password
+
+      expect { within('form') { click_on 'Create Trader Account' } }.to change(User, :count).by(1)
+                                                                    .and change(ActionMailer::Base.deliveries, :count)
+                                                                    .by(1)
+    end
+  end
+
+  context 'when user is not an admin' do
+    it 'raises a routing error' do
+      sign_in approved_trader
+
+      expect { visit new_trader_path }.to raise_error(ActionController::RoutingError)
+    end
+  end
+end
